ad5371d5d08a555f2ebacf97b0798a8251e06b85
Dragan Aleksic <aleksicdragan574@gmail.com>
2023-12-09 12:39:25 +0100

    changed base url for deployment

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://my-json-server.typicode.com/DraganAleksic99/redux-toolkit-RTKQuery'
+  }),
   tagTypes: ['Post'],
   // not using builder variable triggers an eslint warning, but its a required syntax
   // eslint-disable-next-line
